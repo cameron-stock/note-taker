9dce5ea304be2b7b1b8152c6c9fd9032733e8aae
Cameron Stock <clstock33@gmail.com>
2021-02-21 12:54:41 -0600

    init commit

---
+++ src/components/Note.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note() {
+    return ( 
+    <div className="note">
+        <h1>Ahsoka Tano</h1>
+        <p>I am no jedi</p>
+        <p>Grogu</p>
+    </div>
+    );
+}
+
+export default Note;
